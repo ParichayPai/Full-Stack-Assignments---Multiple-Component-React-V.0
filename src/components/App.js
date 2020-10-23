02f95518ad18d7aabd55e34ff30ab89542177101
Parichay Pai <39957257+ParichayPai@users.noreply.github.com>
2020-10-23 14:04:55 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-// import ProjectName from "./ProjectName";
-// import ProjectDescription from "./ProjectDescription";
+import ProjectName from "./ProjectName";
+import ProjectDescription from "./ProjectDescription";
 
 class App extends Component {
   render() {
     return (
       <>
-        {/* <ProjectName key="project-name" />
-        <ProjectDescription key="project-description" /> */}
-        <div key="project-name">Hello There</div>
-        <div key="project-description">General Kenobi</div>
+        <ProjectName key="project-name" />
+        <ProjectDescription key="project-description" />
       </>
     );
   }
